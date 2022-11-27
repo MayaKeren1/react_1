c4eeb1eb12d194a356b9796a5462b53ef38676e0
MayaKeren1 <mayake4@gmail.com>
2022-11-27 14:13:16 +0200

    update

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const data=[
+    {
+        id:1,
+      img:'https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL300_SR300,200_.jpg',
+      title:'I Love You to the Moon and Back',
+      auther:'Amelia Hepworth'
+    },
+    {
+        id: 2,
+      img:'https://images-na.ssl-images-amazon.com/images/I/71RBpTJkKaL._AC_UL300_SR300,200_.jpg',
+      title:'November 9: A Novel',
+      auther:'Colleen Hoover'
+    },
+      {
+        id:3,
+      img:'https://images-na.ssl-images-amazon.com/images/I/71x3UZ18tmL._AC_UL300_SR300,200_.jpg',
+      title:'Im Glad My Mom Died',
+      auther:'Jennette McCurdy'
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom';
+//css
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import {data} from './books';
+import SpecificBook from './Book';
+import {greeting} from './testing/testing';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return(
+    <section className='booklist'>
+      {data.map((book)=>{
+        const {img,title,auther}=book;
+        return(
+          <SpecificBook key={book.id} book={book}></SpecificBook>
+        );
+      })}
+    </section>
+  );
+}
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
